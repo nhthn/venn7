ab5830cabb2b00acb65222148fe021e28a6e4238
Nathan Ho <nathan.waikin.ho@gmail.com>
2020-12-10 21:20:44 -0800

    Refactor rendering into a VennDiagramRenderer class

--- src/venn7/venn.py
+++ src/venn7/venn.py
@@ -17,37 +17,34 @@ from venn7.bezier import BezierPath
 ROOT = pathlib.Path(os.path.realpath(__file__)).parent
 
 class VennDiagram:
-    """A simple, polar symmetric monotone Venn diagram represented with a
-    compact matrix encoding.
+    """A simple symmetric monotone Venn diagram. The diagram is encoded discretely
+    using a set of "row swaps." Creation of path data is performed on the fly.
 
-    Reference: Cao et al. "Symmetric Monoton Venn Diagrams with Seven Curves."
+    See README for more info.
 
-    Glossary
-    --------
+    Parameters
+    ----------
 
-    simple: only two curves may intersect at any point (no triple intersections).
-    polar symmetric: n-fold rotational symmetry, where n is the order of the diagram.
-    monotone: every k-region (region intersecting exactly k curves) is adjacent to a
-    k-1-region if possible and a k+1-region if possible.
-    matrix encoding: an (n-1) by (2^n - 2)/n matrix of 1's and 0's. There is exactly
-    one 1 in every column, the rest are zeros. Each 1 indicates a crossing over of
-    two curves.
-    compact matrix encoding: a list giving the 1-indexed row position of the 1 in each
-    column.
+    n : int
+        The order of the Venn diagram. Must be prime.
+
+    mini_matrix_encoding_string : str
+        A string containing whitespace-separated rows of the "mini matrix encoding."
+        See README for example.
     """
 
-    def __init__(self, n, weave_code, name=None):
+    def __init__(self, n, mini_matrix_encoding_string, name=None):
         self.name = name
         self.n = n
 
-        self.condensed_code = self.parse_weave_code(weave_code)
-        self.code = [y for x in self.condensed_code for y in x]
+        self.row_swaps = self.parse_mini_matrix_encoding_string(mini_matrix_encoding_string)
+        self.flattened_row_swaps = [y for x in self.row_swaps for y in x]
 
         self.validate_basic()
         self.validate_venn()
 
-    def parse_weave_code(self, weave_code):
-        rows = weave_code.strip().split()
+    def parse_mini_matrix_encoding_string(self, mini_matrix_encoding_string):
+        rows = mini_matrix_encoding_string.strip().split()
         matrix = []
         for i, row in enumerate(rows):
             interpolated_row = "0".join(row)
@@ -58,32 +55,32 @@ class VennDiagram:
             interpolated_row = [int(x) for x in interpolated_row]
             matrix.append(interpolated_row)
 
-        code = []
+        row_swaps = []
         for column in range(len(matrix[0])):
-            code_entry = []
+            entry = []
             for row in range(len(matrix)):
                 if matrix[row][column] == 1:
-                    code_entry.append(row + 1)
-            code.append(code_entry)
-        return code
+                    entry.append(row + 1)
+            row_swaps.append(entry)
+        return row_swaps
 
     def validate_basic(self):
-        """Check for basic errors in the matrix code."""
+        """Check for basic errors in the matrix flattened_row_swaps."""
         n = self.n
         expected_length = (2 ** n - 2) // n
-        if len(self.code) != expected_length:
-            raise ValueError(f"Wrong length: code should be of length {expected_length}")
+        if len(self.flattened_row_swaps) != expected_length:
+            raise ValueError(f"Wrong length: flattened_row_swaps should be of length {expected_length}")
 
-        last_x = self.code[-1]
-        for x in self.code:
+        last_x = self.flattened_row_swaps[-1]
+        for x in self.flattened_row_swaps:
             if last_x == x:
-                raise ValueError("Immediate repetitions are not allowed in code")
+                raise ValueError("Immediate repetitions are not allowed in flattened_row_swaps")
             last_x = x
 
         for k in range(1, n - 1):
             expected = math.comb(n, k) // n
             count = 0
-            for x in self.code:
+            for x in self.flattened_row_swaps:
                 if x == k:
                     count += 1
             if count != expected:
@@ -97,7 +94,7 @@ class VennDiagram:
         ranks = [False] * (2 ** n)
         ranks[0] = ranks[-1] = True
         p = list(range(n))
-        for swap_row in self.full_code():
+        for swap_row in self.full_flattened_row_swaps():
             a = swap_row
             b = swap_row - 1
             p[a], p[b] = p[b], p[a]
@@ -108,70 +105,16 @@ class VennDiagram:
         if not all(ranks):
             raise ValueError(f"Not all ranks represented")
 
-    def condense(self):
-        """Condense adjacent swaps"""
-        pass
-
-    def full_code(self):
-        """Return the code duplicated n times."""
-        full_code = []
+    def full_flattened_row_swaps(self):
+        """Return the flattened_row_swaps duplicated n times."""
+        full_flattened_row_swaps = []
         for i in range(self.n):
-            full_code += self.code
-        return full_code
+            full_flattened_row_swaps += self.flattened_row_swaps
+        return full_flattened_row_swaps
 
     def get_spline(self, index=0):
-        grid_points = []
-        row, column = 0, index * len(self.condensed_code)
-        for i in range(self.n):
-            for swap_rows in self.condensed_code:
-                if row + 1 in swap_rows:
-                    grid_points.append((row + 1, column))
-                    row += 1
-                elif row in swap_rows:
-                    grid_points.append((row, column))
-                    row -= 1
-                column += 1
-
-        grid_points_2 = []
-        for i in range(len(grid_points)):
-            r0, c0 = grid_points[(i - 1) % len(grid_points)]
-            r1, c1 = grid_points[i]
-            r2, c2 = grid_points[(i + 1) % len(grid_points)]
-            if not (
-                (c1 - c0) % len(grid_points) == 1
-                and (c2 - c1) % len(grid_points) == 1
-                and r2 - r1 == r1 - r0
-            ):
-                grid_points_2.append((r1, c1))
-        grid_points = grid_points_2
-
-        tensions = []
-        for i in range(len(grid_points)):
-            r1, c1 = grid_points[i]
-            r2, c2 = grid_points[(i + 1) % len(grid_points)]
-            if abs(r1 - r2) == abs(c1 - c2):
-                tensions.append(1.8)
-            else:
-                tensions.append(1.0)
-
-        inner_radius = 30
-        spacing = 5
-
-        control_points = []
-        for row, column in grid_points:
-            radius = inner_radius + spacing * row
-            theta = column * 2 * math.pi / (self.n * len(self.condensed_code))
-            x = radius * math.cos(theta)
-            y = radius * math.sin(theta)
-            control_points.append((x, y))
-
-        spline = MetafontSpline(control_points, tensions)
-
-        # Fudge factor to avoid perfectly coincident endpoints, which cause
-        # issues for Boolean ops.
-        spline = spline.translate(np.array([1e-4, 0]))
-
-        return spline
+        renderer = VennDiagramRenderer(self)
+        return renderer.get_spline()
 
     def get_polygon(self, index=0):
         """Get the shape of a single curve as a polygon."""
@@ -246,6 +189,115 @@ class VennDiagram:
         plt.xlim(-100, 100)
         plt.ylim(-100, 100)
         plt.show()
+
+
+class VennDiagramRenderer:
+    """A class that renders discrete Venn diagrams to splines.
+    """
+
+    def __init__(
+            self,
+            venn_diagram,
+            inner_radius=30,
+            spacing=5,
+            tension_diagonal=1.8,
+            tension_default=1.0,
+        ):
+        self.n = venn_diagram.n
+        self.row_swaps = venn_diagram.row_swaps
+
+        self.inner_radius = inner_radius
+        self.spacing = spacing
+        self.tension_diagonal = tension_diagonal
+        self.tension_default = tension_default
+
+        # Avoid perfectly coincident endpoints, which causes
+        # issues for Boolean ops.
+        self.fudge_factor = 1e-4
+
+    def _get_curve_points_on_cylinder(self, index):
+        """Get the set of control points (not Bezier but Metafont control
+        points) if the Venn diagram were unraveled on a cylinder. All these
+        points lie on a grid."""
+        points = []
+        row, column = 0, index * len(self.row_swaps)
+        for i in range(self.n):
+            for swap_rows in self.row_swaps:
+                if row + 1 in swap_rows:
+                    points.append((row + 1, column))
+                    row += 1
+                elif row in swap_rows:
+                    points.append((row, column))
+                    row -= 1
+                column += 1
+        return points
+
+    def _remove_colinear_points(self, points):
+        """Given a set of control points on the cylinder, find sequences of
+        colinear points in a diagonally ascending or descending pattern.
+        Remove the interiors of these sequences. This results in better behavior
+        of the MetafontSpline.
+        """
+        result = []
+        for i in range(len(points)):
+            r0, c0 = points[(i - 1) % len(points)]
+            r1, c1 = points[i]
+            r2, c2 = points[(i + 1) % len(points)]
+            if not (
+                (c1 - c0) % len(points) == 1
+                and (c2 - c1) % len(points) == 1
+                and r2 - r1 == r1 - r0
+            ):
+                result.append((r1, c1))
+        return result
+
+    def _get_tensions(self, points):
+        """Given a set of control points on the cylinder, determine whether
+        two points are diagonal or horizontal. If they are diagonal, their
+        tension is set to ``tension_diagonal``. Otherwise, their tension is
+        ``tension_default``. Collect a list of all tensions and return it.
+        """
+        tensions = []
+        for i in range(len(points)):
+            r1, c1 = points[i]
+            r2, c2 = points[(i + 1) % len(points)]
+            if abs(r1 - r2) == abs(c1 - c2):
+                tensions.append(self.tension_diagonal)
+            else:
+                tensions.append(self.tension_default)
+        return tensions
+
+    def _convert_cylinder_points_to_polar(self, cylinder_points):
+        polar_points = []
+        for row, column in cylinder_points:
+            radius = self.inner_radius + self.spacing * row
+            theta = column * 2 * math.pi / (self.n * len(self.row_swaps))
+            x = radius * math.cos(theta)
+            y = radius * math.sin(theta)
+            polar_points.append((x, y))
+        return polar_points
+
+    def get_spline(self, index=0):
+        """Render a single curve of the Venn diagram to a BezierSpline
+        and return the result.
+
+        Parameters
+        ----------
+
+        index : int
+            Which curve to return. For a symmetric Venn diagram, indices
+            other than 0 are rotations of each other.
+        """
+        cylinder_points = self._get_curve_points_on_cylinder(index)
+        cylinder_points = self._remove_colinear_points(cylinder_points)
+        tensions = self._get_tensions(cylinder_points)
+
+        control_points = self._convert_cylinder_points_to_polar(cylinder_points)
+        spline = MetafontSpline(control_points, tensions)
+        spline = spline.translate(np.array([self.fudge_factor, 0]))
+
+        return spline
+
 
 DIAGRAMS_LIST = [
     "victoria",
