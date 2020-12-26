18fa7c9e0414d2e122d65f4768ed5e9371886721
Nathan Ho <nathan.waikin.ho@gmail.com>
2020-12-25 22:05:29 -0800

    Refactor JS app, fix cutoff notes, add new sound

--- app/app.js
+++ app/app.js
@@ -56,7 +56,7 @@ const COLOR_SCHEMES = {
         background: [70, 30, 90],
         center: [0, 10, 30],
         foreground: [50, 10, 10],
-        sound: "pad",
+        sound: "weird",
         order: 7
     }),
     // almost black and white, faintly green/yellow
@@ -158,12 +158,33 @@ class VennDiagramApp {
     }
 }
 
+
+function get_venn_sets(region_index, n) {
+    let tmp = region_index;
+    const result = [];
+    let i;
+    for (i = 0; i < n; i++) {
+        result.push(tmp % 2);
+        tmp = Math.floor(tmp / 2);
+    }
+    return result;
+}
+
+
 class VennDiagram {
     constructor(venn_diagram, colorScheme) {
+        this.venn_diagram = venn_diagram;
+        this.n = venn_diagram.n;
+        this.colorScheme = colorScheme;
+
         const canvas_size = 800;
+        this.canvas_size = canvas_size;
         const draw = SVG().addTo("#canvas-container").size(canvas_size, canvas_size);
         this.draw = draw;
         const scale = 350 / 50;
+        this.scale = scale;
+
+        this.player = new VennPlayer(this.n, `sounds/${colorScheme.sound}`);
 
         function updateSize() {
             const size = Math.min(window.innerHeight, window.innerWidth);
@@ -186,80 +207,77 @@ class VennDiagram {
                 .translate(canvas_size / 2, canvas_size / 2);
             return path;
         }
-        function make_region(i) {
-            const path = draw.path(venn_diagram.regions[i])
-                .stroke({ linejoin: "round", linecap: "round" })
-                .scale(scale, 0, 0)
-                .translate(canvas_size / 2, canvas_size / 2)
-            return path;
-        }
 
-        const curves = [];
+        this.curves = [];
         let i;
         for (i = 0; i < venn_diagram.n; i++) {
-            curves.push(make_venn_curve(i));
+            this.curves.push(make_venn_curve(i));
         }
 
-        function get_venn_sets(region_index, n) {
-            let tmp = region_index;
-            const result = [];
-            let i;
-            for (i = 0; i < n; i++) {
-                result.push(tmp % 2);
-                tmp = Math.floor(tmp / 2);
-            }
-            return result;
-        }
-
-        this.synths = [];
-        for (i = 0; i < venn_diagram.n; i++) {
-            const synth = new Tone.Player(`sounds/${colorScheme.sound}/note_${[0, 2, 3, 5, 7, 8, 10][i]}.mp3`).toDestination();
-            synth.volume.value = -10;
-            this.synths.push(synth);
-        }
-
-        curves.forEach((curve) => {
+        this.curves.forEach((curve) => {
             curve.front();
         });
 
         this.regions = [];
         this.region_outlines = [];
         for (i = 1; i < Math.pow(2, venn_diagram.n); i++) {
-            const sets = get_venn_sets(i, venn_diagram.n);
-            const order = get_venn_sets(i, venn_diagram.n).reduce((a, b) => a + b);
-
-            let region = make_region(i);
-            region.fill({ color: colorScheme.regionColors[order] });
-            region.stroke({ width: 0.1, color: colorScheme.regionColors[order] });
-            this.regions.push(region);
-            region.back();
-
-            let region_outline = make_region(i);
-            this.region_outlines.push(region_outline);
-            region_outline
-                .fill({ color: "black", opacity: 0 })
-                .stroke({ opacity: 0, color: colorScheme.foreground, width: 3 / scale });
-
-            region_outline
-                .mouseover(() => {
-                    region_outline.stroke({ opacity: 1 });
-                    let i;
-                    for (i = 0; i < venn_diagram.n; i++) {
-                        curves[i].stroke({ opacity: sets[i] * 0.9 });
-                    }
-                })
-                .mouseout(() => {
-                    region_outline.stroke({ opacity: 0 });
-                })
-                .mousedown(() => {
-                    let i;
-                    for (i = 0; i < venn_diagram.n; i++) {
-                        if (sets[i]) {
-                            this.synths[i].start();
-                        }
-                    }
-                });
+            this.renderRegion(i);
         }
+    }
+
+    makeRegionShape(regionIndex) {
+        const path = this.draw.path(this.venn_diagram.regions[regionIndex])
+            .stroke({ linejoin: "round", linecap: "round" })
+            .scale(this.scale, 0, 0)
+            .translate(this.canvas_size / 2, this.canvas_size / 2)
+        return path;
+    }
+
+    renderRegion(regionIndex) {
+        const sets = get_venn_sets(regionIndex, this.n);
+        const order = get_venn_sets(regionIndex, this.n).reduce((a, b) => a + b);
+
+        let region = this.makeRegionShape(regionIndex);
+        region.fill({
+            color: this.colorScheme.regionColors[order]
+        });
+        region.stroke({
+            width: 0.1,
+            color: this.colorScheme.regionColors[order]
+        });
+        this.regions.push(region);
+        region.back();
+
+        let region_outline = this.makeRegionShape(regionIndex);
+        this.region_outlines.push(region_outline);
+        region_outline
+            .fill({ color: "black", opacity: 0 })
+            .stroke({
+                opacity: 0,
+                color: this.colorScheme.foreground,
+                width: 3 / this.scale
+            });
+
+        region_outline
+            .mouseover(() => {
+                region_outline.stroke({ opacity: 1 });
+                let i;
+                for (i = 0; i < this.n; i++) {
+                    this.curves[i].stroke({ opacity: sets[i] * 0.9 });
+                }
+            })
+            .mouseout(() => {
+                region_outline.stroke({ opacity: 0 });
+            })
+            .mousedown(() => {
+                this.clickRegion(regionIndex);
+            });
+    }
+
+    clickRegion(regionIndex) {
+        console.log("lick");
+        const sets = get_venn_sets(regionIndex, this.n);
+        this.player.playChord(sets);
     }
 
     cleanup() {
@@ -267,11 +285,57 @@ class VennDiagram {
         const node = this.draw.node;
         node.parentElement.removeChild(node);
 
-        for (let synth of this.synths) {
-            synth.dispose();
-        }
+        this.player.cleanUp();
 
         window.removeEventListener("resize", this.resizeListener);
+    }
+}
+
+class VennPlayer {
+    constructor(n, directory) {
+        this.directory = directory;
+        this.n = n;
+        this.polyphony = 3;
+        this.synths = [];
+        for (let i = 0; i < this.n; i++) {
+            let note = [];
+            this.synths.push(note);
+            for (let j = 0; j < this.polyphony; j++) {
+                const synth = new Tone.Player(
+                    `${directory}/note_${[0, 2, 3, 5, 7, 8, 10][i]}.mp3`
+                ).toDestination();
+                synth.fadeOut = 1;
+                synth.volume.value = -10;
+                note.push(synth);
+            }
+        }
+    }
+
+    stopAllSynths() {
+        for (let note of this.synths) {
+            for (let synth of note) {
+                synth.stop();
+            }
+        }
+    }
+
+    playChord(chord) {
+        this.stopAllSynths();
+        for (let i = 0; i < this.n; i++) {
+            if (chord[i]) {
+                let synth = this.synths[i].pop();
+                this.synths[i].unshift(synth);
+                synth.start();
+            }
+        }
+    }
+
+    cleanUp() {
+        for (let note of this.synths) {
+            for (let synth of note) {
+                synth.dispose();
+            }
+        }
     }
 }
 
